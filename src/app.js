43ffdc5c39cec917f80f3e3d035bba6ff0e6bb5d
rgwhite210 <94303284+rgwhite210@users.noreply.github.com>
2022-01-23 22:58:02 -0800

    showing temperature data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 
 function displayTemperature(response){
     console.log(response.data);
-
+    let mainTemp = document.querySelector("#main-temp");
+    mainTemp.innerHTML = Math.round(response.data.main.temp);
+    let city = document.querySelector("#location");
+    city.innerHTML = response.data.name;
+    let status = document.querySelector("#status");
+    status.innerHTML = response.data.weather[0].description;
+    let date = document.querySelector("#date");
+    date.innerHTML = response.data.dt;
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = response.data.wind.speed;
+    
 }
 
 let apiKey = "d3e02cfab692a13c33a6d6d6aec2acb0";
