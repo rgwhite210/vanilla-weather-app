0995b552693ba8fccd59d04af763aec9aff03214
rgwhite210 <94303284+rgwhite210@users.noreply.github.com>
2022-01-23 22:09:45 -0800

    initiated API setup

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ function displayTemperature(response){
 }
 
 let apiKey = "d3e02cfab692a13c33a6d6d6aec2acb0";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 
+console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
