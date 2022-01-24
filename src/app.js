957cd2380fcb812fbbe7923e5465e805a9567e3d
rgwhite210 <94303284+rgwhite210@users.noreply.github.com>
2022-01-24 01:43:21 -0800

    improved forecast functions

--- src/app.js
+++ src/app.js
@@ -15,34 +15,42 @@ function formatDate(timestamp){
     return `${day}, ${hours}:${minutes}`;
 
 }
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+
+    return days[day];
+}
 
 function displayForecast(response){
     console.log(response.data.daily);
-    let forecast = document.querySelector("#forecast");
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+
 
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
             alt=""
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> 18° </span>
-            <span class="weather-forecast-temperature-min"> 12° </span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
           </div>
         </div>
-    `;
+    `;}
     });
   
     forecastHTML = forecastHTML + `</div>`;
-    forecast.innerHTML = forecastHTML;
+    forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates){
     console.log(coordinates);
