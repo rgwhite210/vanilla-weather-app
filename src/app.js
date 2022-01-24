d9364aa7e059bb14ac1d0969089e01f594ddedf4
rgwhite210 <94303284+rgwhite210@users.noreply.github.com>
2022-01-23 23:13:07 -0800

    updated time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 
+function formatDate(timestamp){
+    //calculate the date 
+    let date = new Date(timestamp);
+    let hours = date.getHours()
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
+
+}
+
+
 function displayTemperature(response){
     console.log(response.data);
     let mainTemp = document.querySelector("#main-temp");
     mainTemp.innerHTML = Math.round(response.data.main.temp);
+
     let city = document.querySelector("#location");
     city.innerHTML = response.data.name;
+
     let status = document.querySelector("#status");
     status.innerHTML = response.data.weather[0].description;
-    let date = document.querySelector("#date");
-    date.innerHTML = response.data.dt;
+
     let humidity = document.querySelector("#humidity");
     humidity.innerHTML = response.data.main.humidity;
+
     let wind = document.querySelector("#wind");
     wind.innerHTML = response.data.wind.speed;
+
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate(response.data.dt * 1000);
     
 }
 
